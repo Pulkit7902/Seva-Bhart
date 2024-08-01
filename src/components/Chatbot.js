aef5268c3526860d017fbf6bfe830cde1f439021
Pulkit Chopra <pulkitchopra690@gmail.com>
2024-08-01 17:28:43 +0530

    first-commit

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,16 +1,15 @@
 const Chatbot = () => {
-    window.watsonAssistantChatOptions = {
-      integrationID: "8888e275-6c4c-418b-972c-5e3a164b7638", // The ID of this integration.
-      region: "us-south", // The region your integration is hosted in.
-      serviceInstanceID: "a9d72189-bb5f-45ca-8d97-97347812ae87", // The ID of your service instance.
-      onLoad: async (instance) => { await instance.render(); }
-    };
-    
-    setTimeout(function() {
-      const t = document.createElement('script');
-      t.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
-      document.head.appendChild(t);
-    }, 0); // Add a delay of 0 milliseconds
-  }
+  window.watsonAssistantChatOptions = {
+    integrationID: "8859cd7b-c22e-40a4-964c-5c1be9658ac1", // The ID of this integration.
+    region: "us-south", // The region your integration is hosted in.
+    serviceInstanceID: "a9d72189-bb5f-45ca-8d97-97347812ae87", // The ID of your service instance.
+    onLoad: async (instance) => { await instance.render(); }
+  };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);
+  });
+}
 export default Chatbot
   
